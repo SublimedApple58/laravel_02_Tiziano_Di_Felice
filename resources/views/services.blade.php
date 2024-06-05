79938803f4afc743f125b34989a0e79405e8d006
Tiziano Di Felice <tizianofelice@gmail.com>
2024-06-05 11:09:20 +0200

    aggiunto controller

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -9,26 +9,26 @@
     <link rel="stylesheet" href="./style/style.css">
   </head>
   <body>
-    <!-- inizio navbar -->
-        <nav class="navbar navbar-expand-lg fs-5 navbar-dark bg-dark">
+        <!-- navbar -->
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark fs-5">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/">Navbar</a>
+                <a class="navbar-brand" href="{{ route('homepage') }}">Navbar</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/services">Services</a>
+                            <a class="nav-link active" aria-current="page" href="{{ route('services') }}">Services</a>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="/aboutUs">About us</a>
+                            <a class="nav-link" href="{{ route('aboutUs') }}">About us</a>
                         </li>
                     </ul>
                 </div>
             </div>
-        </nav>
-    <!-- fine navbar -->
+          </nav>
+          <!-- fine navbar -->
     <!-- main -->
         <main class="my-5 py-5">
             <h1 class="text-center">I servizi che offriamo</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,11 @@
 <?php
 
+use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ROTTE
+Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
 
-Route::get('/aboutUs', function () {
-    return view('aboutUs');
-});
+Route::get('/aboutUs', [PublicController::class, 'aboutUs'])->name('aboutUs');
 
-Route::get('/services', function () {
-
-    $items = [
-        [
-            "nome" => "Organizziamo viaggi",
-            "paragrafo" => "organizziamo viaggi per voi senza che vi dobbiate preoccupare di nulla",
-            'img' => "https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        ],
-        [
-            "nome" => "Ti rimborsiamo se serve",
-            "paragrafo" => "organizziamo viaggi per voi senza che vi dobbiate preoccupare di nulla",
-            'img' => "https://images.unsplash.com/photo-1422464804701-7d8356b3a42f?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        ],
-        [
-            "nome" => "Cancellazione gratuita",
-            "paragrafo" => "organizziamo viaggi per voi senza che vi dobbiate preoccupare di nulla",
-            'img' => "https://images.unsplash.com/photo-1570658379397-89d15db1b0de?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        ],
-        [
-            "nome" => "Anche in locale",
-            "paragrafo" => "organizziamo viaggi per voi senza che vi dobbiate preoccupare di nulla",
-            'img' => "https://plus.unsplash.com/premium_photo-1673266203191-bb6471c95268?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        ],
-    ];
-
-    return view(
-        '/services', ["items" => $items]
-    );
-});
+Route::get('/services', [PublicController::class, 'services'])->name('services');
